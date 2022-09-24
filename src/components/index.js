635953a84ab98adc9951feace309cf2ca68e4ecb
Anton <antonandreyev@Antons-MacBook-Air-2.local>
2022-09-24 09:57:47 +0300

    clean up

--- src/components/index.js
+++
@@ -1,2 +0,0 @@
-export { default as QuarklycommunityKitMobileSidePanel } from "./QuarklycommunityKitMobileSidePanel"
-export { default as QuarklycommunityKitNetlifyForm } from "./QuarklycommunityKitNetlifyForm"
